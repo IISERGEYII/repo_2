22afc06bc4c9488cba848c1242883e020aae3162
II <s.kazarnikov@gmail.com>
2024-12-28 14:19:58 +0300

    случайные окружности

--- main.py
+++ main.py
@@ -5,17 +5,16 @@ from PyQt6.QtCore import QPoint
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QWidget, QApplication, QPushButton
 
-
 class Example(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
+        self.btn = QPushButton("Создание круга", self)
         self.setGeometry(500, 300, 600, 600)
         self.point = QPoint(300, 250)
         self.setWindowTitle('Рисование')
-        self.btn = QPushButton('Рисовать', self)
         self.btn.move(250, 500)
         self.do_paint = False
         self.btn.clicked.connect(self.paint)
@@ -33,7 +32,10 @@ class Example(QWidget):
         self.update()
 
     def draw_flag(self, qp):
-        qp.setBrush(QColor(255, 204, 0))
+        random_color_r = random.randrange(0, 256)
+        random_color_g = random.randrange(0, 256)
+        random_color_b = random.randrange(0, 256)
+        qp.setBrush(QColor(random_color_r, random_color_g, random_color_b))
         random_range = random.randrange(5, 200)
         qp.drawEllipse(self.point, random_range, random_range)
 
